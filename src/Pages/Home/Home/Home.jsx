e649f224b34ee03620113690d4dace013aa9b765
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-02-23 16:14:39 +0600

    Custom Font And Color Added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,9 +3,12 @@ import NavBar from "../../Shared/NavBar/NavBar";
 
 const Home = () => {
        return (
-              <div>
+              <div className="bg-greenish">
                      <Helmet><title>E-Commerce Home</title></Helmet>
                      <h2>This Is The Home Page</h2>
+                     <p className="font-logo  text-xl font-bold">Evara</p>
+                     <p className="font-secondary bg-teal-dark">I Am Tonmoy Bosunia</p>
+                     <p className="font-primary bg-mint">Kmn Aso Tumi Tonmoy</p>
                      <NavBar></NavBar>
               </div>
        );
