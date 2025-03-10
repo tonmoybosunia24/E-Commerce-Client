2e02b471d994d998750c384a28df1df4c9de9852
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-03-11 00:33:00 +0600

    Banner Section Fully Complete

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -11,12 +11,21 @@ import subBanner1 from '../../../assets/Sub-Banner/Sub-banner-1.jpg';
 import subBanner2 from '../../../assets/Sub-Banner/Sub-banner-2.jpg';
 import subBanner3 from '../../../assets/Sub-Banner/Sub-bannerM1.jpeg';
 import subBanner4 from '../../../assets/Sub-Banner/Sub-bannerM2.jpeg';
+import categoryProduct1 from '../../../assets/Category-Products/Category-Product-1.jpg'
+import categoryProduct2 from '../../../assets/Category-Products/Category-Product-2.jpg'
+import categoryProduct3 from '../../../assets/Category-Products/Category-Product-3.jpg'
+import categoryProduct4 from '../../../assets/Category-Products/Category-Product-4.jpg'
+import categoryProduct5 from '../../../assets/Category-Products/Category-Product-5.jpg'
+import categoryProduct6 from '../../../assets/Category-Products/Category-Product-6.jpg'
+import categoryProduct7 from '../../../assets/Category-Products/Category-Product-7.jpg'
+import categoryProduct8 from '../../../assets/Category-Products/Category-Product-8.jpg'
 
 const Banner = () => {
        return (
               <div className='bg-aliceBlue py-5 px-5 md:px-10 lg:px-20'>
                      {/* -------------------Swiper Modules----------------- */}
                      <div className='flex flex-col lg:flex-row gap-5'>
+                            {/* ---------------Swiper Modules-------------- */}
                             <Swiper
                                    modules={[Navigation, Pagination, Autoplay]}
                                    spaceBetween={50}
@@ -42,7 +51,7 @@ const Banner = () => {
                                                  <h4 className='text-xs lg:text-2xl'>Big Saving Days Sale</h4>
                                                  <h2 className='text-sm lg:text-4xl'>Women Solid Round <br /> Green T-Shirt</h2>
                                                  <p className='text-sm lg:text-2xl'>Starting At Only <span className='text-Radical font-bold text-md lg:text-3xl'>$59.00</span></p>
-                                                 <button className='bg-Radical text-white text-xs lg:text-lg px-2 lg:px-6 py-1 lg:py-2 rounded-xs lg:rounded-md cursor-pointer'>Shop Now</button>
+                                                 <button className='bg-Radical hover:bg-black text-white text-xs lg px-2 lg:px-6 py-1 lg:py-2 rounded-xs lg:rounded-md cursor-pointer'>Shop Now</button>
                                           </div>
                                    </SwiperSlide>
                                    <SwiperSlide>
@@ -51,7 +60,7 @@ const Banner = () => {
                                                  <h4 className='text-xs lg:text-2xl'>Big Saving Days Sale</h4>
                                                  <h2 className='text-sm lg:text-4xl'>Buy Modern Chair In <br /> Black Color</h2>
                                                  <p className='text-sm lg:text-2xl'>Starting At Only <span className='text-Radical font-bold text-md lg:text-3xl'>$99.00</span></p>
-                                                 <button className='bg-Radical text-white text-xs lg:text-lg px-2 lg:px-6 py-1 lg:py-2 rounded-xs lg:rounded-md cursor-pointer'>Shop Now</button>
+                                                 <button className='bg-Radical hover:bg-black text-white text-xs lg px-2 lg:px-6 py-1 lg:py-2 rounded-xs lg:rounded-md cursor-pointer'>Shop Now</button>
                                           </div>
                                    </SwiperSlide>
                             </Swiper>
@@ -93,6 +102,62 @@ const Banner = () => {
                                           </div>
                                    </div>
                             </div>
+                     </div>
+                     {/* -----------------------Category Slider--------------------- */}
+                     <div className='relative'>
+                            {/* ------------------Category Navigation----------------- */}
+                            <div className='absolute top-1/2 -translate-y-1/2 -left-3 cursor-pointer z-30'><IoIosArrowDropleftCircle className='PrevCategory  text-3xl lg:text-3xl text-Radical' /></div>
+                            <div className='absolute top-1/2 -translate-y-1/2 -right-3 cursor-pointer z-10'><IoIosArrowDroprightCircle className='NextCategory  text-3xl lg:text-3xl text-Radical' /></div>
+                            {/* --------------------Swiper Modules-------------------- */}
+                            <Swiper
+                                   modules={[Navigation]}
+                                   spaceBetween={20}
+                                   slidesPerView={2}
+                                   className=' mt-5'
+                                   navigation={{
+                                          nextEl: '.NextCategory',
+                                          prevEl: '.PrevCategory',
+                                   }}
+                                   breakpoints={{
+                                          640: { slidesPerView: 2 },
+                                          768: { slidesPerView: 4 },
+                                          1024: { slidesPerView: 6 },
+                                   }}
+                            >
+                                   {/* ---------------------Category Slider------------------ */}
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct1} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>Smart Tablet</p></div>
+                                   </SwiperSlide>
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct2} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>Crape T-Shirt</p></div>
+                                   </SwiperSlide>
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct3} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>Leather Watch</p></div>
+                                   </SwiperSlide>
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct4} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>Rolling Diamond</p></div>
+                                   </SwiperSlide>
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct5} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>Wooden Chair</p></div>
+                                   </SwiperSlide>
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct6} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>Sneakers Shoes</p></div>
+                                   </SwiperSlide>
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct7} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>Purse</p></div>
+                                   </SwiperSlide>
+                                   <SwiperSlide className='relative cursor-pointer'>
+                                          <img className='rounded-lg' src={categoryProduct8} alt="" />
+                                          <div className='absolute bottom-5 left-1/2 -translate-x-1/2 w-full text-lg text-center hover:text-Radical'><p>X-Box Controller</p></div>
+                                   </SwiperSlide>
+                            </Swiper>
                      </div>
               </div>
        );
