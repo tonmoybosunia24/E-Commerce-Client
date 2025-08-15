7b789a66b80bed26a05f40222721080ce34ad676
tonmoybosunia24 <mdtonmoybosunia24@gmail.com>
2025-08-15 21:20:00 +0600

    Fix Admin & Moderator Routes Issue

--- src/Routes/AdminRoutes.jsx
+++ src/Routes/AdminRoutes.jsx
@@ -2,17 +2,19 @@ import { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProviders";
 import useAdmin from "../Hooks/useAdmin";
 import { Navigate, useLocation } from "react-router";
+import useModerator from "../Hooks/useModerator";
 
 const AdminRoutes = ({ children }) => {
 
        const { user, loading, Logout } = useContext(AuthContext);
        const [isAdmin, isAdminLoading] = useAdmin();
+       const [isModerator, isModeratorLoading] = useModerator();
        const location = useLocation();
 
-       if (loading || isAdminLoading) {
+       if (loading || isAdminLoading || isModeratorLoading) {
               return <span className="loading loading-spinner text-error flex items-center m-auto min-h-screen"></span>;
        }
-       else if (user && !isAdmin) {
+       else if (user && !isAdmin && !loading && !isAdminLoading) {
               Logout();
               return <Navigate state={{ from: location }} replace to="/login" />;
        }
@@ -20,7 +22,9 @@ const AdminRoutes = ({ children }) => {
               return children;
        }
 
-       return <Navigate state={{ from: location }} replace to='/login'></Navigate>;
+       else if (!loading && !isAdminLoading) {
+              return <Navigate state={{ from: location }} replace to='/login' />;
+       }
 };
 
 export default AdminRoutes;
--- src/Routes/ModeratorRoutes.jsx
+++ src/Routes/ModeratorRoutes.jsx
@@ -11,18 +11,22 @@ const ModeratorRoutes = ({ children }) => {
        const [isModerator, isModeratorLoading] = useModerator();
        const location = useLocation();
 
-       if (loading || isModeratorLoading) {
+       if (loading || isAdminLoading || isModeratorLoading) {
               return <span className="loading loading-spinner text-error flex items-center m-auto min-h-screen"></span>;
        }
-       else if (user && !isModerator && !isAdmin) {
+
+       else if (user && !isModerator && !isAdmin && !loading && !isAdminLoading) {
               Logout();
               return <Navigate state={{ from: location }} replace to="/login" />;
        }
+
        else if ((user && isModerator) || (user && isAdmin)) {
               return children;
        }
 
-       return <Navigate state={{ from: location }} replace to='/login'></Navigate>;
+       else if (!loading && !isAdminLoading && !isModeratorLoading) {
+              return <Navigate state={{ from: location }} replace to='/login' />;
+       }
 };
 
 export default ModeratorRoutes;
